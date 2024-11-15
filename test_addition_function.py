74c4487e41c8e44154e8cdc3ecc7f8c168fcc516
FAAMR <155174366+FAAMR@users.noreply.github.com>
2024-11-15 13:54:45 +0200

    Update test_addition_function.py

--- test_addition_function.py
+++ test_addition_function.py
@@ -4,27 +4,31 @@ from addition_function import add_numbers  # Import the function to test
 
 class TestAdditionFunction(unittest.TestCase):
 
-    def add_positive_numbers(self):
+    def test_add_positive_numbers(self):
         result = add_numbers(2, 3)
-        self.assertEqual(result,5)
+        self.assertEqual(result, 5)
 
-    def add_negative_numbers(self):
+    def test_add_negative_numbers(self):
         result = add_numbers(-1, -3)
-        self.assertEqual(result, -4)
+        self.assertEqual(result, -5)
 
-    def add_positive_and_negative(self):
+    def test_add_positive_and_negative(self):
         result = add_numbers(7, -6)
         self.assertEqual(result, 4)
 
-    def add_zero(self):
+    def test_add_zero(self):
         result = add_numbers(0, 8)
-        self.assertEqual(result, 10)
+        self.assertEqual(result, 8)
         result = add_numbers(8, 0)
         self.assertEqual(result, 8)
 
-    def add_large_numbers(self):
+    def test_add_large_numbers(self):
         result = add_numbers(1000000, 3000000)
         self.assertEqual(result, 4000000)
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 
 if __name__ == '__main__':
